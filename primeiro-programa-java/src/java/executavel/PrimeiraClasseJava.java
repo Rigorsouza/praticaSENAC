f6bf0330e3d96f868c00e2cb266ed325f492ef16
souza <souza@Igor>
2023-05-16 16:04:19 -0300

    criação do projeto

---
+++ primeiro-programa-java/src/java/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package java.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método autoexecutável em Java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, world!!");
+		System.out.println("Java é orientado a objeto.");
+	}
+
+}
